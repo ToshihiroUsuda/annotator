ceb5c9ee9e519caa6f228907853e6f9411a29824
tshrusd <tshr.usd@gmail.com>
2025-05-30 14:44:59 +0900

    vscode git機能のテスト

--- src/services/vott-ct/ts/CanvasTools/Core/Colors/Color.ts
+++ src/services/vott-ct/ts/CanvasTools/Core/Colors/Color.ts
@@ -1,8 +1,8 @@
-import { RGBColor } from './RGBColor'
-import { SRGBColor } from './SRGBColor'
-import { XYZColor } from './XYZColor'
-import { LABColor } from './LABColor'
-import { HSLColor } from './HSLColor'
+import { RGBColor } from "./RGBColor";
+import { SRGBColor } from "./SRGBColor";
+import { XYZColor } from "./XYZColor";
+import { LABColor } from "./LABColor";
+import { HSLColor } from "./HSLColor";
 
 /**
  * A wrapper `Color` class to represent various color formats
@@ -11,100 +11,100 @@ import { HSLColor } from './HSLColor'
  * can be tracked in this draft: https://drafts.csswg.org/css-color/
  */
 export class Color {
-    public get sRGB(): SRGBColor {
-        return this.srgbColor
+  public get sRGB(): SRGBColor {
+    return this.srgbColor;
+  }
+
+  public get RGB(): RGBColor {
+    if (this.rgbColor === undefined) {
+      this.rgbColor = this.srgbColor.toRGB();
     }
 
-    public get RGB(): RGBColor {
-        if (this.rgbColor === undefined) {
-            this.rgbColor = this.srgbColor.toRGB()
-        }
+    return this.rgbColor;
+  }
 
-        return this.rgbColor
+  public get XYZ(): XYZColor {
+    if (this.xyzColor === undefined) {
+      this.xyzColor = this.RGB.toXYZ();
     }
 
-    public get XYZ(): XYZColor {
-        if (this.xyzColor === undefined) {
-            this.xyzColor = this.RGB.toXYZ()
-        }
+    return this.xyzColor;
+  }
 
-        return this.xyzColor
+  public get LAB(): LABColor {
+    if (this.labColor === undefined) {
+      this.labColor = this.XYZ.toLAB();
     }
 
-    public get LAB(): LABColor {
-        if (this.labColor === undefined) {
-            this.labColor = this.XYZ.toLAB()
-        }
+    return this.labColor;
+  }
 
-        return this.labColor
+  public get HSL(): HSLColor {
+    if (this.hslColor === undefined) {
+      this.hslColor = this.srgbColor.toHSL();
     }
 
-    public get HSL(): HSLColor {
-        if (this.hslColor === undefined) {
-            this.hslColor = this.srgbColor.toHSL()
-        }
+    return this.hslColor;
+  }
 
-        return this.hslColor
+  private srgbColor: SRGBColor;
+  private rgbColor: RGBColor | undefined;
+  private xyzColor: XYZColor | undefined;
+  private labColor: LABColor | undefined;
+  private hslColor: HSLColor | undefined;
+
+  constructor(srgb: SRGBColor);
+  constructor(rgb: RGBColor);
+  constructor(hsl: HSLColor);
+  constructor(xyz: XYZColor);
+  constructor(lab: LABColor);
+  constructor(srgbCSSString: string);
+  constructor(r: number, g: number, b: number);
+  constructor(
+    ...args:
+      | [SRGBColor | RGBColor | HSLColor | XYZColor | LABColor | string]
+      | [number, number, number]
+  ) {
+    if (args.length === 1) {
+      const c = args[0];
+      if (c instanceof SRGBColor) {
+        this.srgbColor = c;
+      } else if (c instanceof RGBColor) {
+        this.rgbColor = c;
+        this.srgbColor = c.toSRGB();
+      } else if (c instanceof HSLColor) {
+        this.hslColor = c;
+        this.srgbColor = c.toSRGB();
+      } else if (c instanceof XYZColor) {
+        this.xyzColor = c;
+        this.rgbColor = c.toRGB();
+        this.srgbColor = this.rgbColor.toSRGB();
+      } else if (c instanceof LABColor) {
+        this.labColor = c;
+        this.xyzColor = c.toXYZ();
+        this.rgbColor = this.xyzColor.toRGB();
+        this.srgbColor = this.rgbColor.toSRGB();
+      } else if (typeof c === "string") {
+        this.srgbColor = SRGBColor.ParseHex(c);
+      } else {
+        throw new Error(
+          "Wrong arg type. Expected one of the '***Color' types."
+        );
+      }
+    } else if (args.length === 3) {
+      if (
+        typeof args[0] === "number" &&
+        typeof args[1] === "number" &&
+        typeof args[2] === "number"
+      ) {
+        this.srgbColor = new SRGBColor(args[0], args[1], args[2]);
+      } else {
+        throw new Error(
+          "Wrong arg type. Expected 3 args of the 'number' type."
+        );
+      }
+    } else {
+      throw new Error("Wrong args for Color constructor.");
     }
-
-    private srgbColor: SRGBColor
-    private rgbColor: RGBColor | undefined
-    private xyzColor: XYZColor | undefined
-    private labColor: LABColor | undefined
-    private hslColor: HSLColor | undefined
-
-    constructor(srgb: SRGBColor)
-    constructor(rgb: RGBColor)
-    constructor(hsl: HSLColor)
-    constructor(xyz: XYZColor)
-    constructor(lab: LABColor)
-    constructor(srgbCSSString: string)
-    constructor(r: number, g: number, b: number)
-    constructor(
-        ...args:
-            | [SRGBColor | RGBColor | HSLColor | XYZColor | LABColor | string]
-            | [number, number, number]
-    ) {
-        if (args.length === 1) {
-            const c = args[0]
-            if (c instanceof SRGBColor) {
-                this.srgbColor = c
-            } else if (c instanceof RGBColor) {
-                this.rgbColor = c
-                this.srgbColor = c.toSRGB()
-            } else if (c instanceof HSLColor) {
-                this.hslColor = c
-                this.srgbColor = c.toSRGB()
-            } else if (c instanceof XYZColor) {
-                this.xyzColor = c
-                this.rgbColor = c.toRGB()
-                this.srgbColor = this.rgbColor.toSRGB()
-            } else if (c instanceof LABColor) {
-                this.labColor = c
-                this.xyzColor = c.toXYZ()
-                this.rgbColor = this.xyzColor.toRGB()
-                this.srgbColor = this.rgbColor.toSRGB()
-            } else if (typeof c === 'string') {
-                this.srgbColor = SRGBColor.ParseHex(c)
-            } else {
-                throw new Error(
-                    "Wrong arg type. Expected one of the '***Color' types."
-                )
-            }
-        } else if (args.length === 3) {
-            if (
-                typeof args[0] === 'number' &&
-                typeof args[1] === 'number' &&
-                typeof args[2] === 'number'
-            ) {
-                this.srgbColor = new SRGBColor(args[0], args[1], args[2])
-            } else {
-                throw new Error(
-                    "Wrong arg type. Expected 3 args of the 'number' type."
-                )
-            }
-        } else {
-            throw new Error('Wrong args for Color constructor.')
-        }
-    }
+  }
 }
